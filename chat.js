2b1783b9c451fd43338346abefb8be28e5e38d3f
Pavel <pavel@virtivia.com>
2011-12-11 21:59:51 -0500

    awesome

--- chat.js
+++ chat.js
@@ -1,8 +1,23 @@
-function liberty_launchChat() {
-	var sid = 98323;
-	var wW = 659;
-	var wH = 428;
-	var wL = (window.screen.width - wW) / 2;
-	var wT = (window.screen.height - wH) / 3;
-	window.open('chat.html', '_blank', 'width=' + wW + ',height=' + wH + ',left=' + wL + ',top=' + wT + ',location=no,menubar=no,status=no,toolbar=no');
-}
+$(document).ready(function() {
+	$('#chatbox').keypress(function(e) {
+		if (e.which == 13) { // enter
+			$('#chatbox').val('');
+		}
+	});
+
+	$(window).resize(updateChatLogHeight);
+
+	function updateChatLogHeight() {
+		// disable scrolling as it interferes with calculations and causes visual glitches
+		$('html').css({overflow:'hidden'});
+		var newChatLogHeight = $(window).height() - $('#chatlog').offset().top - 11 - $('#chatboxwrapper').height() - 11;
+		if (newChatLogHeight < 200) {
+			newChatLogHeight = 200;
+			// if the scrollbars are needed, enable them
+			$('html').css({overflow:'auto'});
+		}
+		$('#chatlog').css({height: newChatLogHeight + 'px'});
+	}
+
+	updateChatLogHeight();
+});
