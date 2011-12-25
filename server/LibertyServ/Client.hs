c555cad38ab2dcd4f81813110ccfb664745b5a3f
Pavel <pavel@virtivia.com>
2011-12-25 07:48:58 -0500

    client socket exception handling

--- server/LibertyServ/Client.hs
+++ server/LibertyServ/Client.hs
@@ -1,6 +1,7 @@
 module LibertyServ.Client (
   initializeClient
 ) where
+import Control.Exception
 import qualified Data.ByteString.Lazy as LBS
 import Network.Socket hiding (recv)
 import Network.Socket.ByteString.Lazy (sendAll, recv)
@@ -14,10 +15,25 @@ initializeClient clientSocket = do
   clientSocketLoop clientSocket
 
 clientSocketLoop :: Socket -> IO ()
-clientSocketLoop clientSocket = do
-  -- TODO: Exception
-  recvResult <- recv clientSocket 2048
-  putStrLn $ "Len: " ++ show (LBS.length recvResult)
-  putStrLn $ show $ parseMessage recvResult
-  clientSocketLoop clientSocket
+clientSocketLoop clientSocket = catch
+  (do
+    recvResult <- recv clientSocket 2048
+    if not $ LBS.null recvResult then do
+      putStrLn $ "Len: " ++ show (LBS.length recvResult)
+      let maybeMessage = parseMessage recvResult
+      case maybeMessage of
+        Just (messageType, texts) -> putStrLn $ "MsgType: " ++ show messageType ++ " - Texts: " ++ show texts
+        Nothing -> do
+          putStrLn $ "Client disconnecting -- sent an invalid message"
+          sClose clientSocket
+      clientSocketLoop clientSocket
+    else do
+      putStrLn $ "Client disconnecting -- recv returned nothing"
+      sClose clientSocket
+  )
+  (\ex -> do
+    let _ = ex :: IOException
+    putStrLn $ "Client disconnecting due to IO exception: " ++ show ex
+    sClose clientSocket
+  )
 
