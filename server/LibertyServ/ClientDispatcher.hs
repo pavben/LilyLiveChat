3bc0dad2b3b79464f7b67aa217ad9c1394939198
Pavel <pavel@virtivia.com>
2011-12-25 07:22:12 -0500

    client dispatcher should be safe from exceptions

--- server/LibertyServ/ClientDispatcher.hs
+++ server/LibertyServ/ClientDispatcher.hs
@@ -2,35 +2,47 @@ module LibertyServ.ClientDispatcher (
   runClientDispatcher
 ) where
 import Control.Concurrent
+import Control.Exception
 import Network.Socket
+import Prelude hiding (catch)
 import LibertyServ.Client
 
 runClientDispatcher :: IO ()
-runClientDispatcher = catch toRun onException
+runClientDispatcher = do
+  eitherListenerSocket <- try $ socket AF_INET Stream 0 -- create the socket
+  case eitherListenerSocket of
+    Right listenerSocket ->
+      finally
+      (
+        catch
+        (do
+          initializeListenerSocket listenerSocket 9801
+          acceptLoop listenerSocket
+        )
+        (\ex -> handleException ex)
+      )
+      (sClose listenerSocket) -- close the listener socket regardless of exception being raised
+    Left ex -> handleException ex
   where
-    toRun = do
-      listenerSocket <- initializeListenerSocket 9801
-      acceptLoop listenerSocket
-    onException e = do
-      putStrLn $ "Error in listen/bind/accept: " ++ show e
+    handleException ex = do
+      let _ = ex :: IOException
+      putStrLn $ "Error in listen/bind/accept: " ++ show ex
       putStrLn "Retrying in 5 seconds..."
       -- on failure, wait and try binding again
       threadDelay (5000 * 1000)
       runClientDispatcher
 
-initializeListenerSocket :: PortNumber -> IO Socket
-initializeListenerSocket portNumber = do
-  -- TODO: This can throw an exception
+-- Exceptions handled by caller
+initializeListenerSocket :: Socket -> PortNumber -> IO ()
+initializeListenerSocket listenerSocket portNumber = do
   putStrLn $ "Initializing client listener socket on port " ++ show portNumber
-  listenerSocket <- socket AF_INET Stream 0
   setSocketOption listenerSocket ReuseAddr 1
   bindSocket listenerSocket $ SockAddrInet portNumber iNADDR_ANY
   listen listenerSocket 1000
-  return listenerSocket
 
+-- Exceptions handled by caller
 acceptLoop :: Socket -> IO ()
 acceptLoop listenerSocket = do
-  -- TODO: Handle exceptions
   (clientSocket, clientSockAddr) <- accept listenerSocket
   putStrLn $ "Client connected with address: " ++ show clientSockAddr
   _ <- forkIO $ initializeClient clientSocket
